5dfc4341e188f0e957b9834f498dc85ee1057713
ArtDub <rusartdub@gmail.com>
2019-02-04 10:07:34 +0700

    lab4

--- labs/my_lab_4/main.cpp
+++ labs/my_lab_4/main.cpp
@@ -10,7 +10,7 @@ void failure_func(std::vector<uint32_t> &F, const std::vector<uint32_t> &sp);
 
 void kmp(const std::vector<uint64_t> &string, const std::vector<uint32_t> &F);
 
-uint64_t read_number(bool &EOF_status, uint32_t& column, bool& newline);
+uint64_t read_number(bool &EOF_status, uint32_t &column, uint32_t &row, bool &newline);
 
 int main() {
 
@@ -21,16 +21,39 @@ int main() {
 
     int32_t c;
     uint64_t num = 0;
+    bool last = true;
     do {
         c = getchar();
+    } while (c == ' ');
+    if (c == EOF) {
+        return 0;
+    }
+    do {
+
         if (c == ' ' || c == '\n') {
+            last = false;
             string.push_back(num);
             num = 0;
+            do {
+                c = getchar();
+            } while (c == ' ');
+            if (c == EOF) {
+                return 0;
+            }
+            continue;
         } else {
+            last = true;
             num = (num * 10) + c - '0';
+        }
+        c = getchar();
+        if (c == EOF) {
+            return 0;
         }
     } while (c != '\n');
 
+    if (num != 0 || last) {
+        string.push_back(num);
+    }
 
     z_func(z, string);
     prefix_func(sp, z);
@@ -92,19 +115,25 @@ void failure_func(std::vector<uint32_t> &F, const std::vector<uint32_t> &sp) {
     }
 }
 
-uint64_t read_number(bool &EOF_status, uint32_t& column, bool& newline) {
+uint64_t read_number(bool &EOF_status, uint32_t &column, uint32_t &row, bool &newline) {
     int32_t c;
     uint64_t num = 0;
     do {
         c = getchar();
+        if (c == '\n') {
+            row++;
+        }
+    } while (c == ' ' || c == '\n');
+    do {
+
         if (c == EOF) {
             EOF_status = true;
             break;
         } else if (c == ' ' || c == '\n') {
-            if(c == '\n') {
+            if (c == '\n') {
+                column++;
                 newline = true;
-            }
-            else {
+            } else {
                 column++;
             }
 
@@ -112,22 +141,26 @@ uint64_t read_number(bool &EOF_status, uint32_t& column, bool& newline) {
         } else {
             num = (num * 10) + c - '0';
         }
+        c = getchar();
     } while (true);
     return num;
 }
 
 void kmp(const std::vector<uint64_t> &string, const std::vector<uint32_t> &F) {
-    std::queue<std::pair<uint32_t , uint32_t >> pos;
-    std::pair<uint32_t , uint32_t > pos_pair;
-    uint32_t column = 1, row = 1;
+    std::queue<std::pair<uint32_t, uint32_t >> pos;
+    std::pair<uint32_t, uint32_t> pos_pair;
+    uint32_t column = 0, row = 1;
     uint64_t t;
     uint64_t p = 0, n = string.size();
     bool exit = false;
     bool new_line = false;
-    t = read_number(exit, column, new_line);
+    bool skip = false;
+    t = read_number(exit, column, row, new_line);
+    if (exit)
+        return;
     pos_pair = std::make_pair(row, column);
     pos.push(pos_pair);
-    if(new_line) {
+    if (new_line) {
         column = 0;
         row++;
         new_line = false;
@@ -140,34 +173,38 @@ void kmp(const std::vector<uint64_t> &string, const std::vector<uint32_t> &F) {
             if (exit) {
                 break;
             }
-            t = read_number(exit, column, new_line);
+            t = read_number(exit, column, row, new_line);
             pos_pair = std::make_pair(row, column);
+            if (p == n) {
+                skip = true;
+                std::cout << pos.front().first << ", " << pos.front().second << std::endl;
+            }
             if (pos.size() == n) {
                 pos.pop();
             }
             pos.push(pos_pair);
-            if(new_line) {
+            if (new_line) {
                 column = 0;
                 row++;
                 new_line = false;
             }
         }
 
-        if (p == n) {
+        if (p == n && skip == false) {
             std::cout << pos.front().first << ", " << pos.front().second << std::endl;
         }
-
+        skip = false;
         if (exit)
             break;
 
         if (p == 0) {
-            t = read_number(exit, column, new_line);
+            t = read_number(exit, column, row, new_line);
             pos_pair = std::make_pair(row, column);
             if (pos.size() == n) {
                 pos.pop();
             }
             pos.push(pos_pair);
-            if(new_line) {
+            if (new_line) {
                 column = 0;
                 row++;
                 new_line = false;
