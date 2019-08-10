4e657b6e0df76915c3f0d92591486839b9ba8de9
ArtDub <rusartdub@gmail.com>
2019-08-10 14:03:20 +0700

    huffman codding

---
+++ exam/code/sem_2/greedy_algo/huffman_encoding/python/n.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+
+class HuffmanCodeItem:
+
+    def __init__(self, letter, freq, left=None, right=None):
+        self.letter = letter
+        self.freq = freq
+        self.left = left
+        self.right = right
+
+
+class HuffmanCode:
+
+    def __init__(self, letters, frequencies):
+        self.queue_1 = deque()
+        self.queue_2 = deque()
+        for i in range(len(letters)):
+            self.queue_1.append(HuffmanCodeItem(letters[i], frequencies[i]))
+
+        # building huffman tree
+        while len(self.queue_1) != 0 or len(self.queue_2) != 1:
+            left = self.find_min_and_pop()
+            right = self.find_min_and_pop()
+
+            self.queue_2.append(HuffmanCodeItem('$', left.freq + right.freq, left, right))
+
+    def find_min_and_pop(self):
+        if not self.queue_1:
+            return self.queue_2.popleft()
+
+        if not self.queue_2:
+            return self.queue_1.popleft()
+
+        if self.queue_1[0].freq <= self.queue_2[0].freq:
+            return self.queue_1.popleft()
+        return self.queue_2.popleft()
+
+    def print_ans_aux(self, item, str_collector):
+        if item is None:
+            return
+
+        if item.letter != '$':
+            print(str_collector, end=' ')
+
+        self.print_ans_aux(item.left, str_collector + '0')
+        self.print_ans_aux(item.right, str_collector + '1')
+
+    def print_ans(self):
+        ans = self.queue_2.popleft()
+        self.print_ans_aux(ans, '')
+
+
+if __name__ == '__main__':
+
+    t = int(input())
+    for _ in range(t):
+        l = list(input())
+        f = list(map(int, input().split()))
+        coded = HuffmanCode(l, f)
+        coded.print_ans()
