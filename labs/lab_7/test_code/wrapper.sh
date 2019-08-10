b47ad62c824453414216cfbf6ecd72c5cef46023
ArtDub <rusartdub@gmail.com>
2019-08-10 14:02:00 +0700

    lab 7 report

--- labs/lab_7/test_code/wrapper.sh
+++ labs/lab_7/test_code/wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# *.sh <count of tests> <count of nums in test>
+# *.sh <count of tests>
 
 fail=false
 
@@ -9,13 +9,18 @@ if ! make -C ../; then
     exit 1
 fi
 
-if [[ $# -ne 2 ]]	; then
+if ! make ; then
+    echo "ERROR: Failed to compile file."
+    exit 1
+fi
+
+if [[ $# -ne 1 ]]	; then
     echo "ERROR: Failed in args."
     exit 1
 fi
 
 mkdir -p tests
-if ! python3 test_gen.py $1 $2 ; then
+if ! python3 test_gen.py $1 ; then
     echo "ERROR: Failed to python generate tests."
     exit 1
 fi
@@ -23,22 +28,17 @@ fi
 for test_file in `ls tests/*.t`; do
     answer_file="${test_file%.*}"
     echo "Execute ${test_file}"
-     if !  ../main < ${test_file} > "${answer_file}.my" ; then
+
+    if !  ../main < ${test_file} > "${answer_file}.my" ; then
+     echo "ERROR"
+     continue
+    fi
+
+    if !  ./main < ${test_file} > "${answer_file}.a" ; then
          echo "ERROR"
          continue
-     fi
+    fi
 
-    if ! diff "${answer_file}.a"  "${answer_file}.my" > /dev/null ; then
-        echo "Failed"
-        fail=true
-        test=${answer_file}
-    else
-        echo "OK"
-    fi 
+    diff "${answer_file}.a"  "${answer_file}.my"
 done
 
-
-if $fail ; then
-    printf "\n"
-    echo "ERROR in $test"
-fi
