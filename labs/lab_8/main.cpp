61e258f02478ed98cbb4032b36e0449df026b337
ArtDub <rusartdub@gmail.com>
2019-08-11 12:51:31 +0700

    code style fix

--- labs/lab_8/main.cpp
+++ labs/lab_8/main.cpp
@@ -2,54 +2,31 @@
 #include <algorithm>
 #include <vector>
 
-class sectionItem {
+class TSectionItem {
 public:
     int start;
     int finish;
     int index;
-    sectionItem& operator= (const sectionItem &rhs){
-        start = rhs.start;
-        finish = rhs.finish;
-        index = rhs.index;
-        return *this;
-    }
+
+    TSectionItem &operator=(const TSectionItem &rhs) = default;
 };
 
-bool cmp(sectionItem &lhs, sectionItem &rhs) {
+bool Cmp(TSectionItem &lhs, TSectionItem &rhs) {
     return (lhs.start < rhs.start);
 }
 
-bool cmpIndex(sectionItem &lhs, sectionItem &rhs) {
+bool CmpIndex(TSectionItem &lhs, TSectionItem &rhs) {
     return (lhs.index < rhs.index);
 }
 
+void SegmentSelection(std::vector<TSectionItem> &points, int m) {
+    std::vector<TSectionItem> ans;
 
-
-int main() {
-
-
-    size_t n;
-    std::cin >> n;
-
-    std::vector<sectionItem> pointsNonSort(n);
-
-    std::vector<sectionItem> ans;
-    for (int i = 0; i < n; ++i) {
-        pointsNonSort[i].index = i;
-        std::cin >> pointsNonSort[i].start >> pointsNonSort[i].finish;
-    }
-    std::vector<sectionItem> points = pointsNonSort;
-
-    int m;
-    std::cin >> m;
-
-    std::sort(points.begin(), points.end(), cmp);
-
-    //for (auto k : pointsNonSort) std::cout << k.start << " " << k.finish << " " << k.index << std::endl;
-    //for (auto k : points) std::cout << k.start << " " << k.finish << std::endl;
+    // sort by left(start) point
+    std::sort(points.begin(), points.end(), Cmp);
 
     int index = 0;
-    // pick first section
+    // pick first correct section
     int lastCorrectSection = -1;
     for (; index < points.size(); ++index) {
         if (points[index].start <= 0 && points[index].finish >= 0) {
@@ -61,30 +38,34 @@ int main() {
     // fail
     if (lastCorrectSection == -1) {
         std::cout << 0 << std::endl;
-        return 0;
+        return;
     }
+    //else
     ans.push_back(points[lastCorrectSection]);
 
     //pick the remaining sections
     bool complete = false;
     lastCorrectSection = -1;
     while (index < points.size()) {
+        // find next correct section
         for (; index < points.size(); ++index) {
             if (points[index].start <= ans.back().finish && points[index].finish >= ans.back().finish) {
                 lastCorrectSection = index;
-                if (points[index].finish >= m){
+                // if complete
+                if (points[index].finish >= m) {
                     complete = true;
                     break;
                 }
-            } else if (points[index].start > ans.back().finish) {
+            } else if (points[index].start > ans.back().finish) { // last section was best
                 break;
             }
         }
         //fail
         if (lastCorrectSection == -1) {
             std::cout << 0 << std::endl;
-            return 0;
+            return;
         }
+        //else
         ans.push_back(points[lastCorrectSection]);
         if (m <= ans.back().finish || complete) {
             //complete
@@ -96,12 +77,33 @@ int main() {
 
     if (m <= ans.back().finish) {
         std::cout << ans.size() << std::endl;
-        std::sort(ans.begin(), ans.end(), cmpIndex);
+        // sort by index
+        std::sort(ans.begin(), ans.end(), CmpIndex);
         for (auto j : ans) {
             std::cout << j.start << " " << j.finish << std::endl;
         }
     } else {
         std::cout << 0 << std::endl;
     }
+}
+
+int main() {
+
+
+    size_t n;
+    std::cin >> n;
+
+    std::vector<TSectionItem> points(n);
+
+    for (int i = 0; i < n; ++i) {
+        points[i].index = i;
+        std::cin >> points[i].start >> points[i].finish;
+    }
+
+    int m;
+    std::cin >> m;
+
+    SegmentSelection(points, m);
+
     return 0;
 }
